0f7c743967da5c39ace7e76336f86153d917109b
yjw <woong1103@gmail.com>
2025-08-29 00:39:16 +0900

    two commit

--- src/firstClass02/Main.java
+++
@@ -1,9 +0,0 @@
-package firstClass02;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("hi2");
-	}
-	
-
-}
---
+++ src/firstClass02/Main2.java
@@ -0,0 +1,40 @@
+package firstClass02;
+
+public class Main2 {
+	public static void main(String[] args) {
+		System.out.println("hi2");
+		/*
+		https://www.youtube.com/watch?v=lelVripbt2M
+		https://hackmd.io/@oW_dDxdsRoSpl0M64Tfg2g/ByfwpNJ-K
+		https://learngitbranching.js.org/?locale=ko
+		생활코딩: 'Git'과 'GitHub' 강의로 유명한 곳입니다. 비전공자도 쉽게 이해할 수 있도록 용어를 풀어서 설명해주고, "지옥에서 온 관리자"라는 별명이 있을 정도로 어렵게 느껴지는 Git을 친절하게 알려줍니다.
+
+얄코(Yal.co)의 '제대로 파는 Git & GitHub' (YouTube): Git의 핵심 개념과 GitHub 사용법을 체계적으로 설명하는 유튜브 강의입니다. 유료 강의도 있지만 무료로 제공되는 부분만으로도 충분히 많은 것을 배울 수 있습니다.
+
+코딩애플의 '매우쉽게 알려주는 git & github' (YouTube / 웹사이트): 초보자 눈높이에 맞춰 Git과 GitHub의 개념을 아주 쉽게 설명해주는 무료 강의입니다. 특히 Visual Studio Code(VS Code)에서 GUI를 활용해 Git 기능을 사용하는 방법도 알려줘서 편리합니다.
+
+인프런: 다양한 강사들의 Git/GitHub 강의가 있습니다. '초보자를 위한 git과 Github 시작하기' 등 무료 강의도 많으니 검색해서 자신에게 맞는 강의를 찾아보세요.
+ 개념 정리 블로그
+Inpa Dev: Git의 개념과 원리를 그림과 비유를 사용해 알기 쉽게 설명해주는 블로그입니다. Git의 내부 작동 원리를 깊이 있게 이해하고 싶을 때 유용합니다.
+
+위니북스 / 한빛미디어 블로그: Git과 GitHub의 차이점을 포함해 기본적인 개념들을 이해하기 쉽게 설명한 글들이 있습니다. 용어가 헷갈릴 때 참고하기 좋습니다.
+
+
+		 3. git 설치
+		    1. git config --global [user.name](http://user.name/) "your_name"
+		    2. git config --global user.email "your_email” 
+		    3. git config config --list
+		  이클립스 터미널에서 git bash 열기
+		  해당 워크 플레이스가서 git init
+		  git add . 모든걸 하겠다
+		  git status 어떤 파일 올릴수 있는지 확인
+		  
+		  --히스토리 만들기 -> commit (진짜파일, 진짜짠짜파일, 진짜짜자자자파일)
+		  
+		  
+		  
+		 */
+	}
+	
+
+}
